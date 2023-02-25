1d81746d49878dfac7632c661d9cd94846ed51c6
Kennn <59522126+Kennn-dev@users.noreply.github.com>
2023-02-26 01:04:48 +0700

    update url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  riotUrlVN: 'https://vn2.api.riotgames.com',
-  riotUrlSEA: 'https://sea.api.riotgames.com',
-  riotApiKey: 'RGAPI-0670389f-e6aa-44be-936d-9befbce0d634',
-  dataDragonUrl: 'http://ddragon.leagueoflegends.com',
+  riotUrlVN: "https://vn2.api.riotgames.com",
+  riotUrlSEA: "https://sea.api.riotgames.com",
+  riotApiKey: "RGAPI-0670389f-e6aa-44be-936d-9befbce0d634",
+  dataDragonUrl: "https://ddragon.leagueoflegends.com",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   riotUrlVN: 'https://vn2.api.riotgames.com',
   riotUrlSEA: 'https://sea.api.riotgames.com',
   riotApiKey: 'RGAPI-0670389f-e6aa-44be-936d-9befbce0d634',
-  dataDragonUrl: 'http://ddragon.leagueoflegends.com',
+  dataDragonUrl: 'https://ddragon.leagueoflegends.com',
 };
